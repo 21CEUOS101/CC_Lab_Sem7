6a766173fb478dee746846edeed0ae982a90a6f9
Ashish Prajapati <120437060+21CEUOS101@users.noreply.github.com>
2024-07-01 17:03:10 +0000

    Done Infix to Postfix through Recursion

--- Lab01/InfixToPostfix.java
+++ Lab01/InfixToPostfix.java
@@ -1,94 +1,85 @@
 package Lab01;
+
 import java.util.Scanner;
 import java.util.Stack;
 
-public class InfixToPostfix{
+public class InfixToPostfix {
 
     // Just trying to make it through recursion 
     // Not Main code
-    public static String rec(String query)
-    {
-        if(query.length() == 0)
-        {
-            return "";
-        }
-        else if(query.length() == 1)
-        {
-            return String.valueOf(query.charAt(0));
-        }
 
-        String operand1 = String.valueOf(query.charAt(0));
-        int i = 0;
-
-        if(query.charAt(0) == '(')
-        {
-            i++;
-            StringBuilder sb = new StringBuilder();
-            while(i<query.length() && query.charAt(i) != ')')
-            {
-                sb.append(query.charAt(i));
-                i++;
+    private static String rec(String query, Stack<Character> stack) {
+        if (query.length() == 0) {
+            // Pop all remaining operators in the stack
+            StringBuilder result = new StringBuilder();
+            while (!stack.isEmpty()) {
+                result.append(stack.pop());
             }
-            String brackets = rec(sb.toString());
-            operand1 = brackets;
-            if(i+1 == query.length())
-            {
-                return operand1;
-            }
+            return result.toString();
         }
 
-        Character operator = query.charAt(i+1);
-
-        String operand2 = rec(query.substring(i+2));
-
-        StringBuilder ans = new StringBuilder();
-
-        ans.append(operand1);
-
-        ans.append(operand2);
-
-        ans.append(operator);
-
-        return ans.toString();
-        
+        char ch = query.charAt(0);
+        if (Character.isLetterOrDigit(ch)) {
+            // Directly add operand to result
+            return ch + rec(query.substring(1), stack);
+        } else if (ch == '(') {
+            stack.push(ch);
+            return rec(query.substring(1), stack);
+        } else if (ch == ')') {
+            // Pop from stack until '(' is found
+            StringBuilder result = new StringBuilder();
+            while (!stack.isEmpty() && stack.peek() != '(') {
+                result.append(stack.pop());
+            }
+            if (!stack.isEmpty() && stack.peek() == '(') {
+                stack.pop(); // remove '('
+            }
+            return result.toString() + rec(query.substring(1), stack);
+        } else {
+            // Operator
+            StringBuilder result = new StringBuilder();
+            while (!stack.isEmpty() && precedence(ch) <= precedence(stack.peek())) {
+                if(stack.peek() != '(')
+                {
+                    result.append(stack.pop());
+                }
+                else
+                {
+                    stack.pop();
+                }
+            }
+            stack.push(ch);
+            return result.toString() + rec(query.substring(1), stack);
+        }
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
         String query = sc.next();
 
-        // System.out.println(rec(query));
-
         Stack<Character> st = new Stack<>();
+        // System.out.println(rec(query , st));
         StringBuilder ans = new StringBuilder();
         int i = 0;
 
-        while(i<query.length())
-        {
-            if(query.charAt(i) >= 'a' && query.charAt(i) <= 'z')
-            {
+        while (i < query.length()) {
+            if (query.charAt(i) >= 'a' && query.charAt(i) <= 'z') {
                 ans.append(query.charAt(i));
-            }
-            else
-            {
-                while(!st.isEmpty() && precedence(query.charAt(i))  < precedence(st.peek()))
-                {
+            } else {
+                while (!st.isEmpty() && precedence(query.charAt(i)) < precedence(st.peek())) {
                     Character operator = st.pop();
 
                     ans.append(operator);
                 }
 
-                if(query.charAt(i) != '(' && query.charAt(i) != ')')
-                {
+                if (query.charAt(i) != '(' && query.charAt(i) != ')') {
                     st.push(query.charAt(i));
                 }
             }
             i++;
         }
-        while(!st.isEmpty())
-        {
+        while (!st.isEmpty()) {
             ans.append(st.pop());
         }
         System.out.println(ans.toString());
@@ -96,30 +87,24 @@ public class InfixToPostfix{
         printQuadripleTable(ans.toString());
     }
 
-    public static int precedence(Character c)
-    {
-       return c == '(' ? 9 : c == '^' ? 3 : c == '*' || c == '/' ? 2 : c == '+' || c == '-' ? 1 : -1; 
+    public static int precedence(Character c) {
+        return c == '(' ? 9 : c == '^' ? 3 : (c == '*' || c == '/' || c == '%') ? 2 : c == '+' || c == '-' ? 1 : -1;
     }
 
-    public static void printQuadripleTable(String ans)
-    {
+    public static void printQuadripleTable(String ans) {
         Stack<Character> st = new Stack();
 
-        String result = ans.substring(0,1);
+        String result = ans.substring(0, 1);
 
         System.out.println("Operand1 " + "|" + "Operand2 " + "|" + "Operator |" + "Result");
-        for(int i=1;i<ans.length();i++)
-        {
-            if(ans.charAt(i) >= 'a' && ans.charAt(i) <= 'z')
-            {
+        for (int i = 1; i < ans.length(); i++) {
+            if (ans.charAt(i) >= 'a' && ans.charAt(i) <= 'z') {
                 st.push(ans.charAt(i));
-            }
-            else
-            {
+            } else {
                 String operand1 = result;
                 Character operand2 = st.pop();
 
-                result = ans.substring(0,i+1);
+                result = ans.substring(0, i + 1);
                 System.out.println(operand1 + "\t|" + operand2 + "\t|" + ans.charAt(i) + "\t|" + result);
 
             }
